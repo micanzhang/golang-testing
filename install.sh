0aa5079824d5fe00985a63b4606bcd48ae9ec33c
Mican Zhang <mican.zhang@liulishuo.com>
2017-11-03 13:49:08 +0800

    add gocov-html command

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 apk update && apk add git bash && rm -rf /var/cache/apk/*
 
 go get -u github.com/axw/gocov/gocov
+go get -u gopkg.in/matm/v1/gocov-html
 go get -u github.com/AlekSi/gocov-xml
 go get -u github.com/golang/lint/golint
 # remove source files to reduce image size
