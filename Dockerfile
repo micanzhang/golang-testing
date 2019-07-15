9408aa151b859d1a60eb3f05977403e59c8ad9cf
Mican Zhang <mican.zhang@liulishuo.com>
2019-07-15 14:17:11 +0800

    *: replace alpine to debian

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM golang:1.12-alpine3.9
+FROM golang:1.12
 
 LABEL maintainer="Mican Zhang <micanzhang@gmail.com>"
 
-RUN apk update && apk add gcc libc-dev
+RUN apt-get update && apt-get install gcc libc-dev
 ADD install.sh install.sh
 ADD coverage.sh /usr/bin/coverage
 RUN /bin/sh install.sh && rm install.sh
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-apk update && apk add git bash openssh-client && rm -rf /var/cache/apk/*
+apt-get update && apt-get install git bash openssh-client
 
 go get -u github.com/axw/gocov/gocov
 go get -u gopkg.in/matm/v1/gocov-html
 go get -u github.com/AlekSi/gocov-xml
-go get -u github.com/golang/lint/golint
-go get -u github.com/dominikh/go-tools/cmd/staticcheck
+go get -u golang.org/x/lint/golint
+go get -u honnef.co/go/tools/cmd/staticcheck
 # remove source files to reduce image size
 rm -rf $GOPATH/src/*
